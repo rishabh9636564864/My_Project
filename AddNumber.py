acbe19f04710030452603b6ef67ac9bb361e34c9
Rishabh Upadhyay <uadhyayrishabh@gmail.com>
2025-05-01 18:15:48 +0530

    add no file created

---
+++ AddNumber.py
@@ -0,0 +1,13 @@
+#--This program Add Two Number--
+
+# Solution 1 (With Pre-defined Variables)
+num1 = 4.6
+num2 = 6
+sum = num1+num2
+print("The sum of given number is", sum)
+
+# Solution 2 (With User Input)
+num1 = int(input("Enter a number 1 here: "))
+num2 = float(input("Enter a number 2 here: "))
+sum = num1+num2
+print("The sum of the provided two numbers is", sum)
