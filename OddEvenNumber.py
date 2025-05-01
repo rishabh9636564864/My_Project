3df2bb697b843a049de5101fa3fd486b71baf275
Rishabh Upadhyay <uadhyayrishabh@gmail.com>
2025-05-01 20:23:33 +0530

    Find number is odd or even

---
+++ OddEvenNumber.py
@@ -0,0 +1,6 @@
+#--Is a Number Odd or Even?--
+num = int(input("Enter a number here: "))
+if num % 2 == 0:       # % (Modules operator) to find reminder
+    print("It is an Even number")
+else:
+    print("It is an Odd number")
